94f46535541aa6c4fae9f19f6d1fcad6509e0fad
Christina Hsieh <hsiehkunlun@hotmail.com>
2017-06-27 12:24:17 -0400

    feat: setup routes for user verification

---
+++ server/routes/auth.js
@@ -0,0 +1,62 @@
+const router = require('express').Router();
+const passport = require('passport');
+
+const User = require('../models/User');
+const Verify = require('./verify');
+
+const signup = function(req, res){
+  if (req.body.username.length < 2) {
+    return res.json({err: 'username must be longer than 3 characters'});
+  }
+  User.register(
+    new User({username: req.body.username}),
+    req.body.password,
+    function(err, user){
+      if (err) {
+        return res.status(500).json({err: err});
+      }
+      passport.authenticate('local')(req, res, function(){
+        return res.status(200).json({
+          status: 'Registration Successful!',
+          user: user
+        });
+      });
+    }
+  );
+};
+
+const login = function(req, res){
+  passport.authenticate('local', function(err, user, info){
+    if (err) {
+      return res.status(500).json({err: err});
+    }
+    if (!user) {
+      return res.status(401).json({err: info});
+    }
+    req.logIn(user, function(err){
+      if (err) {
+        return res.status(500).json({err: err});
+      }
+      const token = Verify.getToken({
+        'username': user.username,
+        '_id': user._id
+      });
+      res.status(200).json({
+        status: 'Log in successful.',
+        success: true,
+        token: token
+      });
+    });
+  })(req, res);
+};
+
+const logout = function(req, res) {
+  req.logout();
+  res.status(200).json({status: 'Logout successful.'});
+};
+
+router.post('/signup', signup);
+router.post('/login', login);
+router.post('/logout', logout);
+
+module.exports = router;
---
+++ server/routes/verify.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+const verify = {};
+
+verify.getToken = function(payload) {
+  return jwt.sign(payload, process.env.SECRET, {expiresIn: 3600});
+};
+
+verify.verifyUser = function(req, res, next) {
+  const token = req.body.token || req.headers['x-access-token'];
+  if (token) {
+    jwt.verify(token, process.env.SECRET, function(err, payload){
+      if (err) {
+        return next(err);
+      } else {
+        req.payload = payload;
+        next();
+      }
+    });
+  } else {
+    const err = new Error('No token provided!');
+    err.status = 403;
+    return next(err);
+  }
+};
+
+module.exports = verify;
